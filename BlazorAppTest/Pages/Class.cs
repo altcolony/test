cd19b26de8d718180144b120edeef1d67b89f019
altcolony <altco.re578@gmail.com>
2020-06-25 22:48:27 +0900

    Initial Commit

---
+++ BlazorAppTest/Pages/Class.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace PageToPage
+{
+    public class Class
+    {
+        static public int publicCount = 0;
+    }
+
+    public class JavaScriptInvokable
+    {
+        [JSInvokable]
+        public static Task<string> myfunc()
+        {
+            return Task.FromResult("from C#");
+        }
+    }
+}
